faa0ee5f1e73a66838f4e66b33e2ce8c52f158d9
jin10086 <igaojin@qq.com>
2024-12-02 18:35:09 +0800

    1

--- test/test1.ts
+++ test/test1.ts
@@ -5,42 +5,9 @@ import { MessageData, MessageType, FarcasterNetwork, ReactionType } from '@farca
 
 import { newcast, likecast, recast, castadd } from './rawdata';
 
+import { getproof } from './utils1';
 
 
-function getproof(data: any) {
-  const signatureBuffer = Buffer.from(data.signature, 'base64');
-  
-  // 转换签名的字节序
-  const r = signatureBuffer.slice(0, 32);
-  const s = signatureBuffer.slice(32, 64);  
-
-    // 处理 hash 字段
-    const processedData = {
-      ...data.data,
-      network: FarcasterNetwork.MAINNET // 确保使用正确的枚举值
-    };
-  
-    if (processedData.type === "MESSAGE_TYPE_CAST_ADD" && processedData.castAddBody?.parentCastId) {
-      processedData.castAddBody.parentCastId.hash = Buffer.from(processedData.castAddBody.parentCastId.hash.slice(2), 'hex');
-    } else if (processedData.type === "MESSAGE_TYPE_REACTION_ADD") {
-      processedData.reactionBody.targetCastId.hash = Buffer.from(processedData.reactionBody.targetCastId.hash.slice(2), 'hex');
-      // 转换 reaction type 为枚举值
-      // processedData.reactionBody.type = processedData.reactionBody.type === "REACTION_TYPE_LIKE" ? ReactionType.LIKE : ReactionType.RECAST;
-    }
-
-  // 构造消息数据
-  const message = MessageData.fromJSON(processedData);
-  const messageBytes = MessageData.encode(message).finish();
-
-
-  return {
-    public_key: data.signer,
-    signature_r: "0x" + r.toString('hex'),
-    signature_s: "0x" + s.toString('hex'),
-    message: messageBytes,
-    rawmessage: message
-  };
-}
 
 let newcastproof = getproof(newcast);
 let likecastproof = getproof(likecast);
